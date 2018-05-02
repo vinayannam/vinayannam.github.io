4f1dc4c4a831586bbbb8da9cc174fb43e1177d7d
vinayannam <vinay1011979.7@gmail.com>
2018-05-02 20:04:44 +0530

    Made links

--- app.js
+++ app.js
@@ -1,123 +1,3 @@
-// let resizeReset = function() {
-//     w = canvasBody.width = window.innerWidth;
-//     h = canvasBody.height = window.innerHeight;
-//     setup();
-// };
-
-// const opts = {
-//     particleColor: ['#2C3E50', '#E74C3C', '#ECF0F1', '#3498DB', '#2980B9'],
-//     lineColor: "rgb(200,200,200)",
-//     particleAmount: 50,
-//     defaultSpeed: 1,
-//     variantSpeed: 1,
-//     defaultRadius: 2,
-//     variantRadius: 2,
-//     linkRadius: 200
-// };
-
-// window.addEventListener("resize", function() {
-//     deBouncer();
-// });
-
-// let deBouncer = function() {
-//     clearTimeout(tid);
-//     tid = setTimeout(function() {
-//         resizeReset();
-//     }, delay);
-// };
-
-// let checkDistance = function(x1, y1, x2, y2) {
-//     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-// };
-
-// let linkPoints = function(point1, hubs) {
-//     for (let i = 0; i < hubs.length; i++) {
-//         let distance = checkDistance(point1.x, point1.y, hubs[i].x, hubs[i].y);
-//         let opacity = 1 - distance / opts.linkRadius;
-//         if (opacity > 0) {
-//             drawArea.lineWidth = 0.5;
-//             drawArea.strokeStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${
-//           rgb[2]
-//         }, ${opacity})`;
-//             drawArea.beginPath();
-//             drawArea.moveTo(point1.x, point1.y);
-//             drawArea.lineTo(hubs[i].x, hubs[i].y);
-//             drawArea.closePath();
-//             drawArea.stroke();
-//         }
-//     }
-// };
-
-// Particle = function(xPos, yPos) {
-//     this.x = Math.random() * w;
-//     this.y = Math.random() * h;
-//     this.speed = opts.defaultSpeed + Math.random() * opts.variantSpeed;
-//     this.directionAngle = Math.floor(Math.random() * 360);
-//     this.color = opts.particleColor[Math.floor(Math.random() * opts.particleColor.length)];
-//     this.radius = opts.defaultRadius + Math.random() * opts.variantRadius;
-//     this.vector = {
-//         x: Math.cos(this.directionAngle) * this.speed,
-//         y: Math.sin(this.directionAngle) * this.speed
-//     };
-//     this.update = function() {
-//         this.border();
-//         this.x += this.vector.x;
-//         this.y += this.vector.y;
-//     };
-//     this.border = function() {
-//         if (this.x >= w || this.x <= 0) {
-//             this.vector.x *= -1;
-//         }
-//         if (this.y >= h || this.y <= 0) {
-//             this.vector.y *= -1;
-//         }
-//         if (this.x > w) this.x = w;
-//         if (this.y > h) this.y = h;
-//         if (this.x < 0) this.x = 0;
-//         if (this.y < 0) this.y = 0;
-//     };
-//     this.draw = function() {
-//         drawArea.beginPath();
-//         drawArea.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-//         drawArea.closePath();
-//         drawArea.fillStyle = this.color;
-//         drawArea.fill();
-//     };
-// };
-
-// var particles = [];
-
-// function setup() {
-//     particles = [];
-//     for (let i = 0; i < opts.particleAmount; i++) {
-//         particles.push(new Particle());
-//     }
-//     window.requestAnimationFrame(loop);
-// }
-
-// function loop() {
-//     window.requestAnimationFrame(loop);
-//     drawArea.clearRect(0, 0, w, h);
-//     for (let i = 0; i < particles.length; i++) {
-//         particles[i].update();
-//         particles[i].draw();
-//     }
-//     for (let i = 0; i < particles.length; i++) {
-//         linkPoints(particles[i], particles);
-//     }
-// }
-
-// const canvasBody = document.getElementById("canvas"),
-//     drawArea = canvasBody.getContext("2d");
-// let delay = 200,
-//     tid,
-//     rgb = opts.lineColor.match(/\d+/g);
-// resizeReset();
-// setup();
-
-
-
-
 const canvas = document.getElementById('canvas')
 const c = canvas.getContext('2d')
 
@@ -132,8 +12,29 @@ window.addEventListener('resize', function() {
 })
 
 const prop = {
-    colors: ['#2C3E50', '#E74C3C', '#ECF0F1', '#3498DB', '#2980B9']
+    colors: ['#2C3E50', '#E74C3C', '#ECF0F1', '#3498DB', '#2980B9'],
+    linkRadius: 40
 }
+
+let checkDistance = function(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+};
+
+let linkPoints = function(circle, circles) {
+    for (let i = 0; i < circles.length; i++) {
+        let distance = checkDistance(circle.x, circle.y, circles[i].x, circles[i].y);
+        let opacity = 1 - distance / prop.linkRadius;
+        if (opacity > 0) {
+            c.lineWidth = 0.5;
+            c.strokeStyle = `rgba(200, 200, 200, ${opacity})`;
+            c.beginPath();
+            c.moveTo(circle.x, circle.y);
+            c.lineTo(circles[i].x, circles[i].y);
+            c.closePath();
+            c.stroke();
+        }
+    }
+};
 
 function Circle(x, y, r) {
     const randomNumber = Math.floor(Math.random() * 4)
@@ -208,9 +109,11 @@ function updateAll() {
     for (let i = 0; i < circleArray.length; i++) {
         circleArray[i].update()
     }
+    for (let i = 0; i < circleArray.length; i++) {
+        linkPoints(circleArray[i], circleArray);
+    }
     window.requestAnimationFrame(updateAll)
 }
 
 init()
-
 updateAll()
